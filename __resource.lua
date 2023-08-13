86e34ad7786a77078670150ba70c5bcffa7d29e5
stefaf10 <86800735+stefaf10@users.noreply.github.com>
2023-08-13 15:30:08 +0300

    Create __resource.lua

---
+++ __resource.lua
@@ -0,0 +1,13 @@
+To resource.lua tha einai auto -- Resource Metadata
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+-- Replace 'your-resource-name' with the actual name you want for your resource
+name 'your-resource-name'
+description 'Display text tags above players for Moderators and Admins'
+version '1.0.0'
+
+-- Client Script
+client_script 'client.lua'
+
+-- Replace with any other dependencies your script might have
+dependency 'es_admin2' -- This is just an example, you might need to use the actual dependency name if different
